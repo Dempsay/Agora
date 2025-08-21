ec3b5cb17982ab982005279681255402f444b0d6
Daniel <danielobregon621@gmail.com>
2025-08-21 17:58:35 -0300

    Actualización de la solución Agora y nuevos datos climáticos
    Se actualizó la versión de Visual Studio en el archivo `Agora.sln` a 17.13.35931.197 y se añadió un nuevo proyecto llamado "WebBlazor". También se configuraron las opciones de compilación para este proyecto.
    
    Se creó una nueva interfaz `IGenericService<T>` y su implementación en `GenericService.cs`, junto con una clase estática `ApiEndpoints` para definir endpoints de la API. Se configuró el enrutador en `App.razor` y se inicializó la aplicación Blazor WebAssembly en `Program.cs`.
    
    Se implementó un nuevo diseño en `MainLayout.razor` y un menú de navegación en `NavMenu.razor`, junto con estilos CSS correspondientes. Se añadieron nuevas páginas, incluyendo `Capacitaciones.razor` y `Home.razor`.
    
    Además, se añadieron datos climáticos al archivo `weather.json`, que incluye condiciones para varias fechas, y se mejoró la experiencia de depuración con cambios en `bootstrap.min.css.map`.

---
+++ Service/Services/GenericService.cs
@@ -0,0 +1,51 @@
+﻿using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    internal class GenericService<T> : IGenericService<T> where T : class
+    {
+        private readonly HttpClient _httpClient;
+        public GenericService() { 
+
+        }
+        public Task<T?> AddAsync(T? entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>?> GetAllAsync(string? filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<T>?> GetDeletedsAllAsync(string? filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RestoreAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T? entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
