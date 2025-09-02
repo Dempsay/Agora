985d7550aa9f8f9f774dccbd8eebe082c8792104
Daniel <danielobregon621@gmail.com>
2025-09-02 17:45:40 -0300

    Implementar pantalla de carga y recursos asociados
    Se han realizado varias modificaciones en los archivos del proyecto. En `Desktop.csproj`, se han añadido nuevas configuraciones para la generación automática de recursos y se ha actualizado la referencia a los archivos de recursos. En `Program.cs`, se ha cambiado la vista que se muestra al iniciar la aplicación, pasando de `MenuPrincipalView` a `SplashView`.
    
    En `Resources.Designer.cs`, se ha generado automáticamente una clase de recursos fuertemente tipada, que permite acceder a cadenas traducidas y otros recursos. En `Resources.resx`, se ha añadido un esquema XML que define la estructura de los recursos.
    
    Finalmente, se ha creado la clase `SplashView` en `SplashView.cs` y su diseñador en `SplashView.Designer.cs`, que implementa una pantalla de carga con una barra de progreso y un temporizador.

---
+++ Desktop/Views/SplashView.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Desktop.Views
+{
+    partial class SplashView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashView));
+            ProgressBar = new ProgressBar();
+            Timer = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // ProgressBar
+            // 
+            ProgressBar.Location = new Point(2, 449);
+            ProgressBar.Name = "ProgressBar";
+            ProgressBar.Size = new Size(910, 29);
+            ProgressBar.TabIndex = 0;
+            // 
+            // Timer
+            // 
+            Timer.Enabled = true;
+            Timer.Interval = 500;
+            Timer.Tick += Timer_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Anchor = AnchorStyles.None;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(175, -7);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(564, 495);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // SplashView
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(25, 41, 57);
+            ClientSize = new Size(913, 481);
+            Controls.Add(ProgressBar);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SplashView";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SplashView";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ProgressBar ProgressBar;
+        private System.Windows.Forms.Timer Timer;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Desktop/Views/SplashView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktop.Views
+{
+    public partial class SplashView : Form
+    {
+        public SplashView()
+        {
+            InitializeComponent();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ProgressBar.Value += 3;
+            if (ProgressBar.Value == 100)
+            {
+                Timer.Stop();
+                this.Hide();
+                var menuPrincipalView = new MenuPrincipalView();
+                menuPrincipalView.ShowDialog();
+                this.Close();
+
+
+            }
+        }
+    }
+}
