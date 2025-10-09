c467c7def621b12552ef9f0cc9cd494cd13b4c36
Daniel <danielobregon621@gmail.com>
2025-10-09 18:41:21 -0300

    Añadir gestión de capacitaciones y mejoras en inicio sesión
    Se han creado las vistas `AddEditCapacitacionesView` y `AdminCapacitacionesView` junto con su ViewModel `AdminCapacitacionesViewModel` para permitir la gestión de capacitaciones (agregar, editar y eliminar).
    
    Se han actualizado las referencias en `MovilApp.csproj` para incluir los nuevos archivos XAML.
    
    En `SignInViewModel.cs`, se ha añadido el comando `VolverCommand` para facilitar la navegación de vuelta a la vista de inicio de sesión y se ha mejorado la lógica de validación en el comando de registro.
    
    Se han realizado ajustes en las vistas de inicio de sesión para incluir un botón de volver y se han mejorado la disposición y alineación de los elementos en los archivos XAML.
    
    Además, se han añadido propiedades observables en el `AdminCapacitacionesViewModel` para gestionar la colección de capacitaciones y su estado de refresco.

---
+++ MovilApp/ViewModels/Admin/AdminCapacitacionesViewModel.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Service.Models;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovilApp.ViewModels.Admin
+{
+    partial class AdminCapacitacionesViewModel : ObservableObject
+    {
+        GenericService<Capacitacion> capacitacionService = new();
+
+        [ObservableProperty]
+        private ObservableCollection<Capacitacion> capacitaciones;
+
+        [ObservableProperty]
+        private Capacitacion capacitacionCurrent;
+
+        [ObservableProperty]
+        private bool isRefreshing;
+
+        [ObservableProperty]
+        private string filterText;
+
+        public IRelayCommand AddCommand { get; }
+        public IRelayCommand EditCommand { get; }
+        public IRelayCommand DeleteCommand { get; }
+        public IRelayCommand RefreshCommand { get; }   
+
+        public AdminCapacitacionesViewModel()
+        {
+            _=LoadCapacitaciones();
+            AddCommand = new AsyncRelayCommand(AddCapacitacion);
+            EditCommand = new AsyncRelayCommand(EditCapacitacion, CanEditOrDelete);
+            DeleteCommand = new AsyncRelayCommand(DeleteCapacitacion, CanEditOrDelete);
+            RefreshCommand = new AsyncRelayCommand(LoadCapacitaciones);
+        }
+
+        private async Task DeleteCapacitacion()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanEditOrDelete()
+        {
+            return capacitacionCurrent != null;
+        }
+
+        private async Task EditCapacitacion()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task AddCapacitacion()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task LoadCapacitaciones()
+        {
+            IsRefreshing = true;
+            var capacitaciones = await capacitacionService.GetAllAsync();
+            Capacitaciones = capacitaciones!=null ? new ObservableCollection<Capacitacion>(capacitaciones) : new ObservableCollection<Capacitacion>();
+            IsRefreshing = false;
+        }
+    }
+}
--- MovilApp/Views/Login/SignInView.xaml.cs
+++ MovilApp/Views/Login/SignInView.xaml.cs
@@ -1,9 +1,21 @@
-namespace MovilApp.Views.Login;
-
-public partial class SignInView : ContentPage
+using MovilApp.ViewModels.Login;
+namespace MovilApp.Views.Login
 {
-	public SignInView()
-	{
-		InitializeComponent();
-	}
+
+    public partial class SignInView : ContentPage
+    {
+        public SignInView()
+        {
+            InitializeComponent();
+        }
+        protected override bool OnBackButtonPressed()
+        {
+            if (BindingContext is SignInViewModel vm && vm.VolverCommand.CanExecute(null))
+            {
+                vm.VolverCommand.Execute(null);
+                return true; // Indica que ya manejaste el evento
+            }
+            return base.OnBackButtonPressed();
+        }
+    }
 }
